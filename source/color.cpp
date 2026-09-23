1704a13e4a67a0c9dbe7edab702cd84e87af00ea
Rouex <108469253+Rouex@users.noreply.github.com>
2026-09-23 12:14:25 +0200

    Initial commit

---
+++ source/color.cpp
@@ -0,0 +1,14 @@
+#include "../include/color.h"
+
+void write_color(std::ostream& out, const color& pixel_color){
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Translate the [0,1] component values to they byte range [0,255]
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
